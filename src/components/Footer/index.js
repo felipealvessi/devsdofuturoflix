3b89e787d2fea9255dfad2afd79e521fdb712e18
felipealvessi <felipealvesinfor@mail.com>
2020-07-28 00:34:12 -0300

    modificado o footer para devsdofuturo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png'
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="/">
+        <img className="Logo" src={Logo} alt="Devs do Futuro Logo" />
       </a>
       <p>
         Orgulhosamente criado durante a
@@ -13,6 +14,8 @@ function Footer() {
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
         </a>
+        {' '}
+        ❤
       </p>
     </FooterBase>
   );
