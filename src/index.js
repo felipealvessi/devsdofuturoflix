b0f528fe0220cb1f8d01615e66bf7e8faf008616
felipealvessi <felipealvesinfor@mail.com>
2020-07-31 19:17:30 -0300

    arquivo App renomeado para index.js dentro da pasta pages/home

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/video';
 
 
@@ -16,7 +17,7 @@ const Pagina404 = () => (
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={App} exact />
+      <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route path="/" component={Pagina404} />
     </Switch>
